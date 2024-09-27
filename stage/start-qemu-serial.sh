fba291b730b2cc7eacafe2a337f690e6c0e7d4ee
caliandro-arturo <arturo.caliandro@mail.polimi.it>
2024-09-27 12:29:45 +0200

    handle paths with whitespaces

--- stage/start-qemu-serial.sh
+++ stage/start-qemu-serial.sh
@@ -5,5 +5,5 @@
 # BUG: with -serial stdio it is not possible to stop QEMU with CTRL-A x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-qemu-system-x86_64 -smp 4 -kernel $DIR/bzImage-amd64 -initrd $DIR/initramfs-busybox-amd64.cpio.gz -append "console=ttyS0 init=/init" -vnc :0 -k en-us -serial stdio -serial tcp::6000,server
+qemu-system-x86_64 -smp 4 -kernel "$DIR/bzImage-amd64" -initrd "$DIR/initramfs-busybox-amd64.cpio.gz" -append "console=ttyS0 init=/init" -vnc :0 -k en-us -serial stdio -serial tcp::6000,server
 
